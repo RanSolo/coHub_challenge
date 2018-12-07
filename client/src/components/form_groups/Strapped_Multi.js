f6004a106c2dd9faeebe7ead1924ec20398789ce
Randy Russell <ransolo@me.com>
2018-12-07 11:11:18 -0600

    bring-listfield-back-from-theicloud-grave

--- client/src/components/form_groups/Strapped_Multi.js
+++ client/src/components/form_groups/Strapped_Multi.js
@@ -4,8 +4,13 @@ import { CheckboxField, ListField, ObjectField } from 'react-components-form';
 export default function StrappedMulti({label, options}) {
   return <div className="form-group">
     <label htmlFor={label}>{label}
-      <CheckboxField name={label} label="Yes"
-                     className="form-control checkbox-inline" />
+      <ListField name={label}>
+        <ObjectField>
+          {options.map(selection => (
+          <CheckboxField key={selection} name={selection} label={selection}/>
+        ))}
+        </ObjectField>
+      </ListField>
     </label>
   </div>
 }
