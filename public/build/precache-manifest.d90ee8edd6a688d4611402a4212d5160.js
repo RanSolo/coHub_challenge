d0065f75c901172dca10d339336298ada3d0a900
Randy Russell <ransolo@me.com>
2018-12-09 19:30:01 -0600

    builde

---
+++ public/build/precache-manifest.d90ee8edd6a688d4611402a4212d5160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2b01507e378af0a4514",
+    "url": "/static/css/main.47f0eb33.chunk.css"
+  },
+  {
+    "revision": "f2b01507e378af0a4514",
+    "url": "/static/js/main.f2b01507.chunk.js"
+  },
+  {
+    "revision": "0d76221036d1649cc80e",
+    "url": "/static/js/1.0d762210.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce14c0e73ffc9aa5e208c60c12d03c30",
+    "url": "/static/media/logo.ce14c0e7.svg"
+  },
+  {
+    "revision": "59c35aa5c4edc03f2e4798461d997745",
+    "url": "/index.html"
+  }
+];
